bdd010c48753d181357beb37829cb6aaf84c53e2
komaitaira <komaitaira@gmail.com>
2021-04-24 14:22:12 +0900

    Fix 2回目の呼び出しの順番の修正。同期処理と非同期処理を混ぜない

--- Promise/cache.js
+++ Promise/cache.js
@@ -14,7 +14,8 @@ function parseJSONAsyncWithCache(json, callback) {
   const cached = cache[json];
   console.log(cached)
   if (cached) {
-    callback(cached.err, cached.result);
+    // キャッシュに値が存在する場合でも、非同期的にコールバックを実行する
+    setTimeout(() => callback(cached.err, cached.result), 0);
     return;
   }
   parseJSONAsync(json, (err, result) => {
@@ -39,11 +40,11 @@ parseJSONAsyncWithCache(
 )
 console.log("1回目の呼び出し完了");
 
-// 実行結果は以下のようになる。1回目の呼び出しは結果の出力よりも先に呼ばれているのに対し、2回目の呼び出しは結果の出力の後に呼ばれている。これを修正する必要がある。
+// 実行結果は以下のようになる。コールバックをパラメータとする関数は、それを常に同期的に実行するか、常に非同期的に実行するかのどちらかでなければならない
 
 // undefined
 // 1回目の呼び出し完了
 // 1回目の結果 null { message: 'Hello', to: 'World' }
 // { err: null, result: { message: 'Hello', to: 'World' } }
-// 2回目の結果 null { message: 'Hello', to: 'World' }
 // 2回目の呼び出し完了
+// 2回目の結果 null { message: 'Hello', to: 'World' }
