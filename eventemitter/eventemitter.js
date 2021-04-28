310c057a2285521c5c97751e2d4b394cc396ed2b
komaitaira <komaitaira@gmail.com>
2021-04-28 23:26:45 +0900

    Add FizzBuzz

--- eventemitter/eventemitter.js
+++ eventemitter/eventemitter.js
@@ -1,13 +1,26 @@
 const { EventEmitter } = require("events");
-const eventEmitter = new EventEmitter();
 
-eventEmitter.on("start", () => {
-  console.log("start event!");
-});
+function createFizzBuzzEventEmitter(until) {
+  const eventEmitter = new EventEmitter();
+  _emitFizzBuzz(eventEmitter, until);
+  return eventEmitter;
+}
 
-// This method don't run.
-eventEmitter.on('end', () => {
-  console.log('end event!');
-});
+async function _emitFizzBuzz(eventEmitter, until) {
+  eventEmitter.emit("start");
+  let count = 1;
+  while (count <= until) {
+    await new Promise(resolve => setTimeout(resolve, 100))
+    if (count % 15 === 0) {
+      eventEmitter.emit("FizzBuzz", count);
+    } else if (count % 3 === 0) {
+      eventEmitter.emit("Fizz", count);
+    } else if (count % 5 === 0) {
+      eventEmitter.emit("Buzz", count);
+    }
+    count += 1;
+  }
+  eventEmitter.emit("end");
+}
 
-eventEmitter.emit('start');
+createFizzBuzzEventEmitter(20);
