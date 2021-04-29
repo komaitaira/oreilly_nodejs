0a8fb8e7de94445b7ccaa8532315d8ed5c0edc5b
komaitaira <komaitaira@gmail.com>
2021-04-29 13:20:12 +0900

    プロセス間通信

--- section4/multi-process.js
+++ section4/multi-process.js
@@ -10,4 +10,10 @@ const cpuCount = require('os').cpus().length;
 for (let i = 0; i < cpuCount; i++ ){
   const sub = fork();
   console.log('サブプロセス', sub.process.pid);
+  // IPCでサブプロセスにポート番号を送信
+  sub.send(3000);
+  // IPCで受信したメッセージをハンドリング
+  sub.on('message', ({ pid, response }) => {
+    console.log(process.pid, `${pid}が${response}を返します`);
+  })
 }
