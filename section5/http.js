d94e7c84ff38f7c4da1bfb6abbbb68489036cbf6
komaitaira <komaitaira@gmail.com>
2021-04-29 14:57:40 +0900

    リクエスト関数作成

--- section5/http.js
+++ section5/http.js
@@ -19,3 +19,10 @@ const server = http.createServer((req, res) => {
   }
   res.end();
 }).listen(3000); // 3000番ポートでリクエストを待機
+
+http.request('http://localhost:3000/api/todos', res => {
+  let responseData = '';
+  console.log('statusCode', res.statusCode);
+  res.on('data', chunk => responseData += chunk);
+  res.on('end', () => console.log('responseData', JSON.parse(responseData)));
+}).end();
