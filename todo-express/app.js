c97315d17e58b0fa1d78b42c50ffc322ca5ceaa1
komaitaira <komaitaira@gmail.com>
2021-04-29 16:52:23 +0900

    サーバ起動確認

---
+++ todo-express/app.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+
+let todos = [
+  { id: 1, title: 'ネーム', conpleted: false },
+  { id: 2, title: '下書き', conpleted: true }
+];
+
+const app = express();
+
+// Todo一覧の取得
+app.get('/api/todos', (req, res) => res.json(todos));
+
+app.listen(3000);
