e8f5c55156c09da3224c21d19d9d509ddc201d17
komaitaira <komaitaira@gmail.com>
2021-04-29 17:17:39 +0900

    GETリクエストのハンドリング

--- todo-express/app.js
+++ todo-express/app.js
@@ -2,13 +2,19 @@
 const express = require('express');
 
 let todos = [
-  { id: 1, title: 'ネーム', conpleted: false },
-  { id: 2, title: '下書き', conpleted: true }
+  { id: 1, title: 'ネーム', completed: false },
+  { id: 2, title: '下書き', completed: true }
 ];
 
 const app = express();
 
 // Todo一覧の取得
-app.get('/api/todos', (req, res) => res.json(todos));
+app.get('/api/todos', (req, res) => {
+  if (!req.query.completed) {
+    return res.json(todos);
+  }
+  const completed = req.query.completed === 'true';
+  res.json(todos.filter(todo => todo.completed === completed));
+});
 
 app.listen(3000);
