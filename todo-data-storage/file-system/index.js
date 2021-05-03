d7e3a85d2fe4a03908c3bfd6125ef4afbff8a6cc
komaitaira <komaitaira@gmail.com>
2021-05-03 11:55:21 +0900

    createの実装

--- todo-data-storage/file-system/index.js
+++ todo-data-storage/file-system/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const { exname, extname } = require('path');
-const { readdir, readFile } = require('fs').promises;
+const { readdir, readFile, writeFile } = require('fs').promises;
 
 exports.fetchAll = async () => {
   // 同一ディレクトリ内に存在するJSONファイルを全て取得
@@ -15,4 +15,7 @@ exports.fetchAll = async () => {
 
 exports.fetchByCompleted = completed => exports.fetchAll()
   .then(all => all.filter(todo => todo.completed === completed))
+
+exports.create = todo =>
+  writeFile(`${__dirname}/${todo.id}.json`, JSON.stringify(todo));
 
